3fee8db65542984d5e424bd7c2e1fce4e8cc6714
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-10-27 21:34:40 +0530

    Complete code added

---
+++ BottomList Card Animation/CardViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CardViewController.swift
+//  BottomList Card Animation
+//
+//  Created by Prashant G on 10/27/18.
+//  Copyright © 2018 MyOrg. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    @IBOutlet weak var handleArea: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
